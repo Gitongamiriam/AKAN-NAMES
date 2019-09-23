a520840df5c4c8f7e883f3d26da6bd8b1ea4704a
Gitongamiriam <miriamgitonga2019@gmail.com>
2019-09-23 10:47:08 +0300

    make changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# AKAN-NAMES
+#Project Author
+Miriam Njeri Gitonga 
+#Project Description
+Akan Ghananian Naming Culture is frequently naming their children after the day they were born and the order in
+which they were born
---
+++ css/scripts.js
@@ -0,0 +1,29 @@
+
+var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"];
+function checkgender() {
+    var month =parseInt(document.getElementById("month").value);
+    var year =parseInt(document.getElementById("year").value);
+    var day=parseInt(document.getElementById("day").value);
+    var gender=checkout();
+    var date=new Date(year + "/" + month + "/" + day);
+    var day=date.getDay();
+    var akan; 
+    if(gender === "male") {
+        akan =akanMale[day];
+    }
+        else{
+            akan =akanFemale[day];
+        }
+            alert("You were born on " + dayOfTheWeek [day] + "and your akan name is " + akan);
+}
+function checkout(){
+    var gender=document.getElementsByName("gender");
+    for(i=0; i<gender.length; i++){
+        if(gender[i].checked){
+            return(gender[i].value)
+        }
+    }
+}
+
